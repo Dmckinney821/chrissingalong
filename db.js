92922ef0bc24ec82df9b6682f4315c336aaf0402
Dan McKinney <3dhdaniel@gmail.com>
2018-06-22 13:42:04 -0400

    adds getFinished

--- db.js
+++ db.js
@@ -12,30 +12,36 @@ const cn = {
 const db = pgp(cn);
 function getTodo(id) {
 return db.oneOrNone('SELECT * FROM todos where id=$1', [id])
-}
+ }
 function getAll() {
-    return db.any('select * from todos');
-}
+    return db.any('select * from todos');}
 getAll()
     .then((data) => {console.log(data);})
     .catch((error) => {console.log(error);});
-// getTodo(2)
-// .then(function(data) {
-//     console.log(data);
-// })
-//      .catch(function(error) {
-//         console.log(error);
-//     });
+getTodo(2)
+.then(function(data) {
+    console.log(data);
+})
+     .catch(function(error) {
+        console.log(error);
+    });
 
-// getTodo(2);
+getTodo(2);
 function getPending () {
     return db.any('select * from todos where isdone=false');
 }
 getPending()
     .then((data) => {console.log(data);})
     .catch((error) => {console.log(error);});
+function getFinished() {
+    return db.any('select * from todos where isdone=true');}
+
+getFinished()
+    .then((data) => {console.log(data);})
+    .catch((error) => {console.log(error);});
 module.exports = {
     getTodo,
     getAll,
-    getPending
+    getPending,
+    getFinished
 };
