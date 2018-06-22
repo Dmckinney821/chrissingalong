c234739ad00a0d5fd54bc428243c9c948e28bd87
Dan McKinney <3dhdaniel@gmail.com>
2018-06-22 14:56:41 -0400

    route handlers

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = experss();
+const app = express();
 
 const Todo = require('./db');
 // only using route handlers so no handlebars here
-app.get('/', (req, res) => {Todo.getALl()
+app.get('/', (req, res) => {Todo.getAll()
     .then((data) => {
         console.log(data);
         res.send(data);
