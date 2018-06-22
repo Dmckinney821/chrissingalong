d5ef4bd6123a287d5f01612d46744f12fb5fd324
Dan McKinney <3dhdaniel@gmail.com>
2018-06-22 13:30:57 -0400

    push it real good

--- db.js
+++ db.js
@@ -11,17 +11,25 @@ const cn = {
 };
 const db = pgp(cn);
 function getTodo(id) {
-db.oneOrNone('SELECT * FROM todos where id=$1'
-, [id])
-.then(function(data) {
-    console.log(data);
-})
-    .catch(function(error) {
-        console.log(error);
-    });
-};
-getTodo(2);
+return db.oneOrNone('SELECT * FROM todos where id=$1', [id])
+}
+function getAll() {
+    return db.any('select * from todos');
+}
+getAll()
+    .then((data) => {console.log(data);})
+    .catch((error) => {console.log(error);});
+// getTodo(2)
+// .then(function(data) {
+//     console.log(data);
+// })
+//      .catch(function(error) {
+//         console.log(error);
+//     });
+
+// getTodo(2);
 
 module.exports = {
-    getTodo
+    getTodo,
+    getAll
 };
