374fd5fa004eb0ec31a26e6d5e4100b2b2ce0be4
Dan McKinney <3dhdaniel@gmail.com>
2018-06-25 12:04:09 -0400

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 
+
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 const expressHbs = require('express-handlebars');
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -25,7 +27,18 @@ app.get('/new', (req, res) => {
         res.render('todo-create-page');
     })
     
+
+app.post('/new', (req, res) => {console.log(req.body);
+    // res.send('Hey you submitted the form')
+    Todo.add(req.body.title)
+    .then((data) => {
+        console.log(data);
+        res.send(data);
+    });
+
+});
     
+
 
 app.get('/:id', (req, res) => {
     Todo.getTodo(req.params.id)
