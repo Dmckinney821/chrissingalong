6d032928894bc43e0575b13f29c6ac78e346644c
Dan McKinney <3dhdaniel@gmail.com>
2018-06-25 09:58:49 -0400

    renders and views and layouts

--- dummydata.sql
+++ dummydata.sql
@@ -4,5 +4,11 @@ values
 	('take out trash', false),
 	('cook dinner', true),
 	('scoop the poop', true)
+	('party', false),
+	('bullshit', true),
+	('dgaf', true)
+	('pasta sex', false),
+	('lasagna wings', true),
+	('gluten classico', true)
 	;
 
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
+const expressHbs = require('express-handlebars');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 const Todo = require('./db');
 // only using route handlers so no handlebars here
 app.get('/', (req, res) => {Todo.getAll()
     .then((data) => {
         console.log(data);
-        res.send(data);
+        // res.send(data);
+        res.render('homepage', {
+            todos: data
+        });
     })
     .catch((error) => {
         console.log(error);});
